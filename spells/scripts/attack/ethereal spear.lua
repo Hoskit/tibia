ad3c6c491132a9303a5c112fa35ac8f5b5f97bb5
tonyye <tonyye333@gmail.com>
2017-05-19 18:25:01 +0200

    Changes level / 5 to level * 2 for all skills

--- spells/scripts/attack/ethereal spear.lua
+++ spells/scripts/attack/ethereal spear.lua
@@ -6,7 +6,7 @@ combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ETHEREALSPEAR)
 
 function onGetFormulaValues(player, level, attack, factor)
 	local distanceSkill = player:getEffectiveSkillLevel(SKILL_DISTANCE)
-	local skillDouble, levelTotal = distanceSkill * 2, level / 5
+	local skillDouble, levelTotal = distanceSkill * 2, level * 2
 	return -(((skillDouble + attack / 3500) * 0.35) + (levelTotal) + 0), -(((skillDouble + attack / 3125) * 0.5) + (levelTotal) + 5)
 end
 

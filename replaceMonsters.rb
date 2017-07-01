1d241ecb40759e97a8c25ad7e2880b453e62017b
Tony Ye <tony.ye@trustly.com>
2017-07-01 19:37:02 +0200

    Increased monster health substaintially

--- replaceMonsters.rb
+++ replaceMonsters.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 
-files = Dir.glob("npc/*.xml")
-regex = "([1-8][0-9]{3})"
+files = Dir.glob("monster/*/*.xml")
+regex = "([1-9][0-9]{1,3})"
 
 files.each do |file|
 	aFile = File.open(file, "r")
@@ -10,9 +10,9 @@ files.each do |file|
 
 	experience = aString[/experience="([0-9]+)"/, 1].to_i
 
-	if experience > 2000
-		doubleFieldSize = aString[/health now="#{regex}" max="#{regex}"/, 1].to_i * 1.2
-		doubleFieldSize2 = aString[/health now="#{regex}" max="#{regex}"/, 2].to_i * 1.2
+	if experience > 0
+		doubleFieldSize = aString[/health now="#{regex}" max="#{regex}"/, 1].to_i * 1.3
+		doubleFieldSize2 = aString[/health now="#{regex}" max="#{regex}"/, 2].to_i * 1.3
 
 		doubleFieldSize = doubleFieldSize.to_i
 		doubleFieldSize2 = doubleFieldSize2.to_i
